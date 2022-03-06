18a51c7fe140ee063892d1fdec807cc7a24a9292
Pranav <pra168109@gmail.com>
2022-03-06 17:18:47 -0600

    changed socketio to http

--- src/contexts/socket.js
+++ src/contexts/socket.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import socketio from 'socket.io-client';
 
-const SERVER_URL = '143.198.53.176:3001';
+const SERVER_URL = 'http://143.198.53.176:3001';
 export const socket = socketio.connect(SERVER_URL, {
     withCredentials: true,
 });
