aded9fb5dd13b569fc2d4692f9573b51aeabcf83
Pranav <pra168109@gmail.com>
2022-03-06 21:39:08 -0600

    removed withCredentials from socket

--- src/contexts/socket.js
+++ src/contexts/socket.js
@@ -4,6 +4,5 @@ import socketio from 'socket.io-client';
 const SERVER_URL = 'http://143.198.53.176:3001';
 export const socket = socketio.connect(SERVER_URL, {
     secure: true,
-    withCredentials: true,
 });
 export const SocketContext = React.createContext();
